e6f128963f150418115d7e2401d477964dddcafe
Vinicius Macedo <viniciusmacedo010@outlook.com>
2025-03-12 19:24:52 -0300

    COMMIT

--- AULA3/Faker.php
+++ AULA3/Faker.php
@@ -7,10 +7,11 @@ require_once 'vendor/autoload.php';
 $faker = Faker\Factory::create('pt_BR');
 $faker->addProvider(new Faker\Provider\pt_BR\Text($faker));
 // Gerar dados falsos
-echo "Nome: " . $faker->name() . "\n";
-echo "Endereço: " . $faker->address() . "\n";
-echo "Email: " . $faker->email() . "\n";
-echo "Texto: " . $faker->realtext(200) . "\n";
+echo "Nome: " . $faker->name() . "<br>";
+echo "Endereço: " . $faker->address() . "<br>";
+echo "Email: " . $faker->email() . "<br>";
+echo "Texto: " . $faker->realText(500) . "<br>";
+
 
 
 // para rodar esse código acima e necessário rodar o seguinte comando no terminal - php faker.php''
