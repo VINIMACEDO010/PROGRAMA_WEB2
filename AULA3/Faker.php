77c9220770f3e6ce72f3f864b0408d2824231667
Vinicius Macedo <viniciusmacedo010@outlook.com>
2025-03-12 21:15:16 -0300

    COMMIT

--- AULA3/Faker.php
+++ AULA3/Faker.php
@@ -1,20 +1,18 @@
 <?php
 
-// Autoload do Composer
+
 require_once 'vendor/autoload.php';
 
-// Criar uma instância do Faker
+
 $faker = Faker\Factory::create('pt_BR');
 $faker->addProvider(new Faker\Provider\pt_BR\Text($faker));
-// Gerar dados falsos
+
 echo "Nome: " . $faker->name() . "<br>";
 echo "Endereço: " . $faker->address() . "<br>";
 echo "Email: " . $faker->email() . "<br>";
 echo "Texto: " . $faker->realText(500) . "<br>";
 
-
-
-// para rodar esse código acima e necessário rodar o seguinte comando no terminal - php faker.php''
+// rodar o comando no terminal - php faker.php''
 
 ?> 
 
